2e10cb7927183523485aca2fc17f6d562cd47701
Jennifer Rush <jennifer.rush2@gmail.com>
2023-05-03 17:02:29 -0600

    create and test cell methods before render

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,10 +2,43 @@ require 'pry'
 require './ship'
 
 class Cell
-    attr_reader :coordinate
+    attr_reader :coordinate, :ship, :attacked
 
     def initialize(coordinate)
         @coordinate = coordinate
+        @ship = []
+        @attacked = false
     end
+
+    def empty?
+        if ship == []
+            true
+        else
+            false
+        end
+    end
+
+    def place_ship(new_ship)
+        ship << new_ship
+        return ship
+    end
+
+    def fired_upon?
+        if attacked == false
+            false
+        else
+            true
+        end
+    end
+
+    def fire_upon
+        @attacked = true
+        if ship.empty? == false
+            ship[0].hit
+        end
+    end
+
+
     
+
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,5 +8,51 @@ RSpec.describe Cell do
 
         expect(cell).to be_an_instance_of(Cell)
     end
-    
+
+    it "has a coordinate" do
+        cell = Cell.new("B3")
+
+        expect(cell.coordinate).to eq("B3")
+    end
+
+    it "starts without a ship" do
+        cell = Cell.new("B3")
+
+        expect(cell.ship).to eq([])
+    end
+
+    it "starts empty" do
+        cell = Cell.new("B3")
+
+        expect(cell.empty?).to eq(true)
+    end
+
+    it "can place a ship in the cell" do
+        cell = Cell.new("B3")
+        cruiser = Ship.new("Cruiser", 3)
+        cell.place_ship(cruiser)
+
+        expect(cell.ship).to eq([cruiser])
+        expect(cell.empty?).to eq(false)
+    end
+
+    it "starts having not been fired upon" do
+        cell = Cell.new("B3")
+        cruiser = Ship.new("Cruiser", 3)
+        cell.place_ship(cruiser)
+
+        expect(cell.fired_upon?).to eq(false)
+    end
+
+    it "can be fired upon" do
+        cell = Cell.new("B3")
+        cruiser = Ship.new("Cruiser", 3)
+        cell.place_ship(cruiser)
+        cell.fire_upon
+        
+        expect(cell.fired_upon?).to eq(true)
+        expect(cruiser.health).to eq(2)
+    end
+
+
 end
