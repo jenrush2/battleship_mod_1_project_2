c7a03351173bc7fb5397dbd15e70a59720c35b66
Jennifer Rush <jennifer.rush2@gmail.com>
2023-05-17 18:03:30 -0600

    Add player shot to Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,6 +18,30 @@ class Turn
         player_board.render(true)
     end
 
+    def player_shot(shot_coordinate)
+        coord = computer_board.cells[shot_coordinate]
+
+        if computer_board.valid_coordinate?(coord.coordinate) == false
+            p "That is not a valid shot. Please try again."
+        else
+            if coord.fired_upon? == true
+                turn_result = "You've fired on #{coord.coordinate} before. It was already a #{coord.render(true)}. Way to waste a turn."
+            else 
+                coord.fire_upon
+                if coord.render(true) == "H"
+                    turn_result = "Your shot on #{coord.coordinate} was a hit!"
+                elsif coord.render(true) == "M"
+                    turn_result = "Your shot on #{coord.coordinate} was a miss!"
+                elsif coord.render(true) == "X"
+                    turn_result = "Your shot on #{coord.coordinate} was a hit! You sunk the #{coord.ship[0].name}!"
+                end
+            p turn_result
+            return turn_result
+            end
+            turn_result
+        end
+    end
+
 
 end
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -61,4 +61,95 @@ RSpec.describe Turn do
         #check printout for display
     end
 
+    it "lets the player take a shot that's a hit" do
+        comp_board = Board.new
+        comp_cruiser = Ship.new("Cruiser", 3)
+        comp_submarine = Ship.new("Submarine", 2)
+        comp_cruiser_array = ["A1", "B1", "C1"]
+        comp_submarine_array = ["D3", "D4"]
+
+
+        comp_board.place(comp_cruiser, comp_cruiser_array)
+        comp_board.place(comp_submarine, comp_submarine_array)
+
+        test_player_board = Board.new
+        test_player_cruiser = Ship.new("Cruiser", 3)
+        test_player_submarine = Ship.new("Submarine", 2)
+        test_player_cruiser_array = ["C2", "C3", "C4"]
+        test_player_submarine_array = ["A3", "B3"]
+
+        test_player_board.place(test_player_cruiser, test_player_cruiser_array)
+        test_player_board.place(test_player_submarine, test_player_submarine_array)
+
+
+        turn = Turn.new(comp_board, test_player_board)
+        expect(turn.player_shot("A1")).to eq("Your shot on A1 was a hit!")
+
+    end
+
+    it "lets the player take a shot that's a miss" do
+        comp_board = Board.new
+        comp_cruiser = Ship.new("Cruiser", 3)
+        comp_submarine = Ship.new("Submarine", 2)
+        comp_cruiser_array = ["A1", "B1", "C1"]
+        comp_submarine_array = ["D3", "D4"]
+
+
+        comp_board.place(comp_cruiser, comp_cruiser_array)
+        comp_board.place(comp_submarine, comp_submarine_array)
+
+        test_player_board = Board.new
+        test_player_cruiser = Ship.new("Cruiser", 3)
+        test_player_submarine = Ship.new("Submarine", 2)
+        test_player_cruiser_array = ["C2", "C3", "C4"]
+        test_player_submarine_array = ["A3", "B3"]
+
+        test_player_board.place(test_player_cruiser, test_player_cruiser_array)
+        test_player_board.place(test_player_submarine, test_player_submarine_array)
+
+
+        turn = Turn.new(comp_board, test_player_board)
+        expect(turn.player_shot("C4")).to eq("Your shot on C4 was a miss!")
+
+    end
+
+    it "lets the player take a shot that sinks a ship" do
+        comp_board = Board.new
+        comp_cruiser = Ship.new("Cruiser", 3)
+        comp_submarine = Ship.new("Submarine", 2)
+        comp_cruiser_array = ["A1", "B1", "C1"]
+        comp_submarine_array = ["D3", "D4"]
+
+
+        comp_board.place(comp_cruiser, comp_cruiser_array)
+        comp_board.place(comp_submarine, comp_submarine_array)
+
+        test_player_board = Board.new
+        test_player_cruiser = Ship.new("Cruiser", 3)
+        test_player_submarine = Ship.new("Submarine", 2)
+        test_player_cruiser_array = ["C2", "C3", "C4"]
+        test_player_submarine_array = ["A3", "B3"]
+
+        test_player_board.place(test_player_cruiser, test_player_cruiser_array)
+        test_player_board.place(test_player_submarine, test_player_submarine_array)
+
+
+        turn = Turn.new(comp_board, test_player_board)
+        turn.player_shot("D3")
+
+        expect(turn.player_shot("D4")).to eq("Your shot on D4 was a hit! You sunk the Submarine!")
+
+    end
+
+
+    xit "lets the computer take a smart shot" do
+    end
+    xit "gives the results of both shots" do
+    end
+    xit "lets you know if the coordinates have already been fired upon" do
+    end
+    xit "checks to see if the game is over" do
+    end
+
+
 end
