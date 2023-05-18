a97cd8bce42c86e1d3bcba8ea3193c3ab2bdde3f
Jennifer Rush <jennifer.rush2@gmail.com>
2023-05-18 14:39:36 -0600

    add computer smart shot with no hits yet

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,6 +42,84 @@ class Turn
         end
     end
 
+    def computer_shot
+        array_of_board_coordinates = player_board.cells.keys
+        
+        array_of_hits = []
+        array_of_board_coordinates.each do |cell|
+            if player_board.cells[cell].render == "H"
+                array_of_hits << cell
+                array_of_hits
+            end
+            array_of_hits
+        end
+
+        if array_of_hits != [] 
+            array_of_smart_guess_options = []
+            array_of_hits.each do |cell|
+                letter_split_as_number = cell[0].ord 
+                letter_above = letter_split_as_number + 1
+                letter_below = letter_split_as_number - 1
+                smart_guess_1 = "#{letter_above.chr}#{cell[1]}"
+                smart_guess_2 = "#{letter_below.chr}#{cell[1]}"
+
+                number_split = cell[1]
+                number_above = number_split + 1
+                number_below = number_split - 1
+                smart_guess_3 = "#{cell[0]}#{number_above}"
+                smart_guess_4 = "#{cell[0]}#{number_below}"
+
+                array_of_smart_guess_options << [smart_guess_1, smart_guess_2, smart_guess_3, smart_guess_4]
+                array_of_smart_guess_options = array_of_smart_guess_options.flatten
+
+                array_of_smart_guess_options_on_board = []
+
+                array_of_smart_guess_options.each do |coord|
+                    if player_board.cells.key?(coord) == true and coord.fired_upon? == false
+                        array_of_smart_guess_options_on_board << coord
+                        array_of_smart_guess_options_on_board
+                    end
+                    array_of_smart_guess_options_on_board
+                end
+                
+                array_of_smart_guess_options_on_board
+            end
+
+            computer_smart_guess = array_of_smart_guess_options_on_board.sample(1)
+            p "line 89 #{computer_smart_guess}"
+            computer_smart_guess = player_board.cells[computer_smart_guess]
+            p "line 91 #{computer_smart_guess}"
+
+            computer_smart_guess
+        else
+            computer_smart_guess = array_of_board_coordinates.sample(1)
+            p "line 96 #{computer_smart_guess}"
+            computer_smart_guess = player_board.cells[computer_smart_guess[0]]
+            p "line 98 #{computer_smart_guess}"
+            computer_smart_guess
+        end
+
+        computer_smart_guess.fire_upon
+        if computer_smart_guess.render(true) == "H"
+            turn_result = "My shot on #{computer_smart_guess.coordinate} was a hit!"
+        elsif computer_smart_guess.render(true) == "M"
+            turn_result = "My shot on #{computer_smart_guess.coordinate} was a miss!"
+        elsif computer_smart_guess.render(true) == "X"
+            turn_result = "My shot on #{computer_smart_guess.coordinate} was a hit! I sunk your #{coord.ship[0].name}!"
+        end
+        
+        p turn_result
+        
+
+
+
+        turn_result
+
+
+    end
+            
+
+
 
 end
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -142,8 +142,34 @@ RSpec.describe Turn do
     end
 
 
-    xit "lets the computer take a smart shot" do
+    it "lets the computer take a smart shot when there hasn't been a hit yet" do
+        comp_board = Board.new
+        comp_cruiser = Ship.new("Cruiser", 3)
+        comp_submarine = Ship.new("Submarine", 2)
+        comp_cruiser_array = ["A1", "B1", "C1"]
+        comp_submarine_array = ["D3", "D4"]
+
+
+        comp_board.place(comp_cruiser, comp_cruiser_array)
+        comp_board.place(comp_submarine, comp_submarine_array)
+
+        test_player_board = Board.new
+        test_player_cruiser = Ship.new("Cruiser", 3)
+        test_player_submarine = Ship.new("Submarine", 2)
+        test_player_cruiser_array = ["C2", "C3", "C4"]
+        test_player_submarine_array = ["A3", "B3"]
+
+        test_player_board.place(test_player_cruiser, test_player_cruiser_array)
+        test_player_board.place(test_player_submarine, test_player_submarine_array)
+
+
+        turn = Turn.new(comp_board, test_player_board)
+
+        expect(turn.computer_shot).to eq("My shot on A1 was a hit!")
+
     end
+
+
     xit "gives the results of both shots" do
     end
     xit "lets you know if the coordinates have already been fired upon" do
